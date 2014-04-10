6991731233f05ac606400d1feafd8ad6cac1f40d
robert <robert@robert-mbp.local>
2014-04-10 14:32:20 -0700

    strategy can create stormpath client

--- test/index.js
+++ test/index.js
@@ -1,9 +1,13 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var lib = require('../lib');
+
 var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
+var StormpathStrategy = proxyquire('../lib/strategy',{'stormpath':require('./mocks/sp')});
 
 var MockSpApp = require('./mocks/sp-app');
+var MockSpClient = require('./mocks/sp-client');
 
 describe('test framework', function(){
   it('should assert true', function(){
@@ -12,62 +16,104 @@ describe('test framework', function(){
 });
 
 describe('lib entry', function(){
-  it('should export an function', function(){
-    assert.equal(typeof lib,"function");
-  });
-  it('should export a constructor', function(){
-    assert.equal(typeof lib.Strategy,"function");
-  });
-  it('export and constructor are same thing', function(){
-    assert.equal(lib,lib.Strategy);
-  });
+    var entry = require('../lib');
+    it('should export an function', function(){
+        assert.equal(typeof entry,"function");
+    });
+    it('should export a constructor', function(){
+        assert.equal(typeof entry.Strategy,"function");
+    });
+    it('export and constructor are same thing', function(){
+        assert.equal(entry,entry.Strategy);
+    });
+});
+
+describe('strategy entry', function(){
+    it('should export an function', function(){
+        assert.equal(typeof StormpathStrategy,"function");
+    });
 });
 
 describe('Strategy instance', function(){
-    it('should throw without spApp',function(){
-        expect(function(){return new lib();}).throws(/^spApp not provided$/);
-    });
+
     it('should have an authenticate method',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         assert.equal(typeof instance.authenticate, "function");
     });
+    it('should define a serializeUser rmethod',function(){
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
+        assert.equal(typeof instance.serializeUser, "function");
+    });
+    it('should define a deserializeUser rmethod',function(){
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
+        assert.equal(typeof instance.deserializeUser, "function");
+    });
     it('should call fail if an empty request body provided',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         var fail = sinon.spy();
         instance.fail = fail;
         expect(instance.authenticate.bind(instance,require('./mocks/req').empty)).to.not.throw();
         assert(fail.called,'fail was not called');
     });
     it('should call fail if a malformed request body provided',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         var fail = sinon.spy();
         instance.fail = fail;
         expect(instance.authenticate.bind(instance,require('./mocks/req').malformed)).to.not.throw();
         assert(fail.called,'fail was not called');
     });
     it('should call fail if body params are not strings',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         var fail = sinon.spy();
         instance.fail = fail;
         expect(instance.authenticate.bind(instance,require('./mocks/req').notStrings)).to.not.throw();
         assert(fail.called,'fail was not called');
     });
     it('should support custom fields',function(){
-        var instance = new lib({spApp:new MockSpApp(),usernameField:"un",passwordField:"pw"});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient(),
+            usernameField:"un",
+            passwordField:"pw"
+        });
         var success = sinon.spy();
         instance.success = success;
         expect(instance.authenticate.bind(instance,require('./mocks/req').custom)).to.not.throw();
         assert(success.called,'success was not called');
     });
     it('should call success if valid login is provieded',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         var success = sinon.spy();
         instance.success = success;
         expect(instance.authenticate.bind(instance,require('./mocks/req').good)).to.not.throw();
         assert(success.called,'success was not called');
     });
     it('should call fail and not success if invalid login is provieded',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         var fail = sinon.spy();
         var success = sinon.spy();
         instance.fail = fail;
@@ -76,13 +122,38 @@ describe('Strategy instance', function(){
         assert(fail.called,'fail was called');
         assert(!success.called,'success was called but shouldnt have been');
     });
+    it('should be instantiabnle without defining a client or app',function(){
+        expect(function(){return new StormpathStrategy({});}).to.not.throw();
+    });
+    it('should be instantiabnle with a defined app, but no client',function(){
+        expect(function(){return new StormpathStrategy({spApp:new MockSpApp()});}).to.not.throw();
+    });
+
+    it('should return the correct data from serializeUser',function(){
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
+        var cb = sinon.spy();
+        instance.serializeUser({href:"a"},cb);
+        assert(cb.calledWith(null,"a"));
+    });
+
+    it('should return the correct data from deserializeUser',function(){
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
+        var cb = sinon.spy();
+        instance.deserializeUser("a",cb);
+        assert(cb.calledWith(null,{href:"a"}));
+    });
+
     it('should have a name property',function(){
-        var instance = new lib({spApp:new MockSpApp()});
+        var instance = new StormpathStrategy({
+            spApp:new MockSpApp(),
+            spClient: new MockSpClient()
+        });
         assert.equal(instance.name, "stormpath");
     });
 });
-
-// TODO
-// no username and password
-// bad username and password
-// user is found
